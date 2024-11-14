a001ec7627db5587258336ce0e7670a99807eae7
Eric <eric@example.com>
2024-11-14 12:01:13 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+//document.getElementById("count-e1").innerText =115;
+
+//let myAge = 50;
+//console.log(myAge);
+
+//let firstBatch =5;
+//let secondBatch =7;
+//let count = firstBatch+ secondBatch;
+
+//console.log(count);
+
+
+//control + j -> terminal
+
+let count =0;
+let countE1 =document.getElementById("count-e1");
+
+
+function increment()
+{
+    count = count +1;
+ 
+    countE1.innerHTML =count;
+}
+
+function save(){
+    console.log(count)
+    let countStr =count +
+    // to check whether this line contains any bugs
+    console.log(count);
+}
